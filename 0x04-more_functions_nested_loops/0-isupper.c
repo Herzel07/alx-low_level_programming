d62aeaf555eedace8fc7107439a862d53833dd60
Herzel07 <herzeldandile@gmail.com>
2023-02-23 11:17:12 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * isupper - checks for upper charecter
+ * @c: number to be checd
+ * Return:1 if c is upper
+ * 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >=65 $$ c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
