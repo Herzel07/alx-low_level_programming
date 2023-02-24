a987221bde22cf3927d647740df2a1bfe3fcc54c
Herzel07 <herzeldandile@gmail.com>
2023-02-24 11:39:59 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * main - print a fizz-buzz test program
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+
+	if ((x % 3 == 0) && (x % 5 == 0))
+
+	printf("FizzBuzz");
+
+	else if (x % 3 == 0)
+
+	printf("Fizz");
+
+	else if (x % 5 == 0)
+
+	printf("Buzz");
+	printf('\n');
+	return (0);
+}
+
+
+
