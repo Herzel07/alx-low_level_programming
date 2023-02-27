ff834b926557e431d328ef5ca10c8c94dfd44187
Herzel07 <herzeldandile@gmail.com>
2023-02-27 11:41:26 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: pring in rev
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0
+
+	while (str[a] != '\0')
+		b++;
+
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+}
+	_putchar('\n');
