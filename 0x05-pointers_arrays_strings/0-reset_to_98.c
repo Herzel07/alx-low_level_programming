df8e0604b7d79ecdfef4a2101e45c17a558ac1ba
Herzel07 <herzeldandile@gmail.com>
2023-02-27 08:41:08 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - a function that takes a pointer to an int as parameter
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 98
+	int ptr = &n
+
+	printf("%d\n ", n);
+	return (0);
+}
+
