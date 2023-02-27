dfe7fda645dfff740cad5d3d7a4f40f52dfd8405
Herzel07 <herzeldandile@gmail.com>
2023-02-27 12:18:43 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string -  reverses a string
+ * @s: reversing strings
+ * Return: 0;
+ */
+
+void rev_string(char *s)
+{
+	int a, b;
+
+	b = 0;
+
+	while (s[b] != '\0')
+		b++;
+
+	for (a = b - 1; a >= 0; a--)
+
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
