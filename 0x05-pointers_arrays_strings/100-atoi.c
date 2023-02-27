24728f61810220e89e9ffdf728a59af801f0f39e
Herzel07 <herzeldandile@gmail.com>
2023-02-27 14:35:52 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do{
+	do {
 		if (*s == '_')
 			sign *= -1;
 
@@ -19,8 +19,7 @@ int _atoi(char *s)
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
-	}
+	} while (*s++);
 
-	while (*s++);
 	return (num + sign);
 }
