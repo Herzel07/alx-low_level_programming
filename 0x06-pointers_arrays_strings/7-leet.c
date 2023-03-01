de607118b5bf8231279661ddcc5109a73235c265
Herzel07 <herzeldandile@gmail.com>
2023-03-01 12:54:36 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,11 +5,11 @@
  * @n: input
  * Return: n
  */
-char *leet(char *)
+char *leet(char *n)
 {
 	int i, j;
-	char s1[] = "aAeEoOtTlL"
-	char s2[] = "4433007711"
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
