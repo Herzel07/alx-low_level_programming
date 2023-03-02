093e8210cf702b586b91f9afb92f8195f6154838
Herzel07 <herzeldandile@gmail.com>
2023-03-02 10:40:55 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,7 +17,7 @@ int main(void)
  * You are not allowed to use ,
  * You are not allowed to code anything else than the expected line of code
  */
-*(p + 5) = 98;
+p + 5 = 98;
 /* ...so that this can print 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
