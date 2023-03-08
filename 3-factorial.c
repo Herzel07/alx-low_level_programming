f421aca337000154b19b9aeb21a237c98a5fab43
Herzel07 <herzeldandile@gmail.com>
2023-03-08 08:38:29 -0800

    main

---
+++ 3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given numbe
+ * @n: the number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
