c6740717e25929d8abb59a9469d3a0deec667a26
Herzel07 <herzeldandile@gmail.com>
2023-03-10 12:57:35 -0800

    static_libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Description - checks for uppercase character.
+ * @c: number to be checd
+ * Return:1 if c is upper
+ * 0 otherwise
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
