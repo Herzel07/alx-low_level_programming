540311b32b520660ba29c99cbdb88a0bd7e4c27f
Herzel07 <herzeldandile@gmail.com>
2023-03-16 06:30:26 -0700

    main

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 	if (n < p2)
 		s = malloc(sizeof(char) * (p1 + n + 1));
 	else
-		s malloc(sizeof(char) * (p1 + p2 + 1))
+		s malloc(sizeof(char) * (p1 + p2 + 1));
 
 	if (!s)
 		return (NULL);
