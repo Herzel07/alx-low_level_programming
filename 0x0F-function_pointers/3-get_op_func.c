9fb28d6c83f54dcd754a993ab824bb721a4e74b3
Herzel07 <herzeldandile@gmail.com>
2023-03-22 12:49:37 -0700

    main

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stdlib.h>
 
 /**
  * get_op_func - function that selects the correct function to perform.
@@ -9,12 +10,12 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
     };
     int i;
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -12,10 +12,10 @@ int op_add(int a, int b)
 }
 
 /**
- * opp_add - perform the operation asked by the user.
+ * opp_sub - perform the operation asked by the user.
  * @a: first integer
  * @b: second integer
- * Return: sum of a and b
+ * Return: differences of a and b
  */
 int op_sub(int a, int b)
 {
@@ -23,10 +23,10 @@ int op_sub(int a, int b)
 }
 
 /**
- * opp_add - perform the operation asked by the user.
+ * opp_mul - perform the operation asked by the user.
  * @a: first integer
  * @b: second integer
- * Return: sum of a and b
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
@@ -34,10 +34,10 @@ int op_mul(int a, int b)
 }
 
 /**
- * opp_add - perform the operation asked by the user.
+ * opp_div - perform the operation asked by the user.
  * @a: first integer
  * @b: second integer
- * Return: sum of a and b
+ * Return: division of a and b
  */
 int op_div(int a, int b)
 {
@@ -45,10 +45,10 @@ int op_div(int a, int b)
 }
 
 /**
- * opp_add - perform the operation asked by the user.
+ * opp_mod - perform the operation asked by the user.
  * @a: first integer
  * @b: second integer
- * Return: sum of a and b
+ * Return: remainder of a divided b
  */
 int op_mod(int a, int b)
 {
