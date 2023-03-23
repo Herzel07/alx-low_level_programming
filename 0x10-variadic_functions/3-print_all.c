9d50f4a7911ebb06fc8082668adc2a6664c49c77
Herzel07 <herzeldandile@gmail.com>
2023-03-23 12:14:44 -0700

    main

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,7 +12,7 @@ void print_all(const char * const format, ...)
 	va_list list;
 	unsigned int i = 0, b, c = 0;
 	char *str;
-	const char t_arg[] = "dat3";
+	const char t_arg[] = "cifs";
 
 	va_start(list, format);
 	while (format && format[i])
@@ -28,16 +28,16 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-			case 'd':
+			case 'c':
 				printf("%c", va_arg(list, int)), c = 1;
 				break;
-			case 'a':
+			case 'i':
                                 printf("%d", va_arg(list, int)), c = 1;
 				break;
-			case 't':
+			case 'f':
                                 printf("%f", va_arg(list, double)), c = 1;
 				break;
-			case '3':
+			case 's':
                                 str = va_arg(list, char *), c = 1;
 				if (!str)
 				{
