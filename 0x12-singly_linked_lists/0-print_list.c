b0efd16ba82ad8640f63e56ba1eeef81d432f7ac
Herzel07 <herzeldandile@gmail.com>
2023-03-30 12:47:19 -0700

    main

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,17 +11,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->l, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		nodes++;
-		h = h->next:
+		h = h->next;
 	}
-	return (nodes):
+	return (nodes);
 }
