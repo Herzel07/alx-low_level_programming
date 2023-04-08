44a9e9639f987bf94e98c0ff92d2ebf9010731d3
Herzel07 <herzeldandile@gmail.com>
2023-04-08 03:26:23 -0700

    main

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,9 +8,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 5))
+	if (index >= (sizeof(unsigned long int) * 10))
 		return (-1);
-	*n != (1 << index);
+	*n = (1 << index);
 
 	return (1);
 }
