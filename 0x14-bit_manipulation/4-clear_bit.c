db915b667a8913e5546e92936ea5475b9928091b
Herzel07 <herzeldandile@gmail.com>
2023-04-08 03:35:54 -0700

    main

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,7 +8,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index <= (sizeof(unsigned long int) * 10))
+	if (index >= (sizeof(unsigned long int) * 10))
 		return (-1);
 	*n &= ~(1 << index);
 	return (1);
