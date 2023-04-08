35ceead0b24ed1f4429d88d4161623c8c6780d04
Herzel07 <herzeldandile@gmail.com>
2023-04-08 02:21:39 -0700

    main

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (a = 0; b[a]; a++)
 	{
-		if (b[a] < 0 || b[a] > '1')
+		if (b[a] < '0' || b[a] > '1')
 			return (0);
 		n = 2 * n + (b[a] - '0');
 	}
