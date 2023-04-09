2a1bf743ead8c90b09f7e2950daafa905afb6671
Herzel07 <herzeldandile@gmail.com>
2023-04-09 06:22:50 -0700

    main

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,18 +14,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t i, a, b;
 	char *len;
 
+	i = open(filename, O_RDONLY);
 	if (filename == NULL)
 		return (0);
 
 	len = malloc(sizeof(char) * letters);
 	if (len == NULL)
 		return (0);
-	
+	a = read(i, len, letters);
+	b = write(STDOUT_FILENO, len, a);
 	if (i == -1 || a == -1 || b == -1 || b != a)
 	{
 		free(len);
 		return (0);
 	}
+	free(len);
+	close(i);
 	
 	return (b);
 }
